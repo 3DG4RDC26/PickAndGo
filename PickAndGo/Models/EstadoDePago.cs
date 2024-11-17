e860a7b92ca57b9ff731ef41e2a213d67ad90868
abojii <abojorgec@gmail.com>
2024-11-17 17:31:25 -0600

    Clases creadas

---
+++ PickAndGo/Models/EstadoDePago.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PickAndGo.Models
+{
+    internal class EstadoDePago
+    {
+        public string Estado { get; set; }
+        public string Comentarios { get; set; }
+    }
+}
